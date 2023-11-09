eafc2a8af6c8189e214c35bf26a04d96f02bb19a
Марина Романова <marina.simaganova@gmail.com>
2023-11-09 13:35:42 +0400

    test-commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>Hi there!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Новый текст
         </p>
         <a
           className="App-link"
